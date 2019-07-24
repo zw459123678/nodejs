6c2742b439ba7a11dc4a00be596c01a74f0a600c
Zeng wei (曾威) <zeng32@qq.com>
2019-07-24 15:47:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
-const config = require('./config')
 
-app.get('/', (req, res) => res.send('Hello222!' + config.server))
+var server = 'server'
+
+try{
+    const config = require('./config')
+    server = config.server
+} catch(err) {
+
+}
+
+app.get('/', (req, res) => res.send('Hello222!' + server))
 
 app.listen(8500, () => console.log('Example app listening on port 8500!'))
