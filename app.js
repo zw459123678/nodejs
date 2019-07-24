7eb8955f131c1df5d219a5489021f68755e549d9
Zeng wei (曾威) <zeng32@qq.com>
2019-07-24 15:34:41 +0800

    test
    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const config = require('./config')
 
-app.get('/', (req, res) => res.send('Hello222!'))
+app.get('/', (req, res) => res.send('Hello222!' + config.server))
 
 app.listen(8500, () => console.log('Example app listening on port 8500!'))
